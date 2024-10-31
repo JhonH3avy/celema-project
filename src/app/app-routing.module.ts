243a298e799eb21e2ab84ae77fe2d594ffa8fc46
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-10-31 08:45:02 -0500

    Add Equiment Admin page (#5)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ProfileAdminComponent } from './features/general/profile-admin/profile-
 import { RoleAdminComponent } from './features/general/role-admin/role-admin.component';
 import { ProductAdminComponent } from './features/general/product-admin/product-admin.component';
 import { ProductFamilyAdminComponent } from './features/general/product-family-admin/product-family-admin.component';
+import { EquipmentAdminComponent } from './features/general/equipment-admin/equipment-admin.component';
 
 const routes: Routes = [
   { path: 'administracion-perfiles', component: ProfileAdminComponent },
   { path: 'administracion-roles', component: RoleAdminComponent },
   { path: 'administracion-productos', component: ProductAdminComponent },
   { path: 'administracion-familia-productos', component: ProductFamilyAdminComponent },
+  { path: 'administracion-equipos', component: EquipmentAdminComponent },
 ];
 
 @NgModule({
---
+++ src/app/features/general/equipment-admin/equipment-admin.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-admin',
+  templateUrl: './equipment-admin.component.html',
+  styleUrls: ['./equipment-admin.component.css']
+})
+export class EquipmentAdminComponent implements OnInit {
+
+  equimentCount = 0;
+
+  currentPage = 1;
+  totalPages = 5;
+  pages = Array(this.totalPages).fill(0);
+
+  equipments = [
+    {
+      id: 1,
+      code: 'PF1',
+      type: 'TP1',
+      description: 'Esta es la descripción de la familia del producto',
+      status: 'Activo',
+      creationDate: new Date('2023-07-06'),
+      location: 'ZN1',
+    },
+    {
+      id: 2,
+      code: 'PF2',
+      type: 'TP2',
+      description: 'Esta es la descripción de la familia del producto',
+      status: 'Activo',
+      creationDate: new Date('2023-07-06'),
+      location: 'ZN2',
+    },
+    {
+      id: 3,
+      code: 'PF3',
+      type: 'TP1',
+      description: 'Esta es la descripción de la familia del producto',
+      status: 'Activo',
+      creationDate: new Date('2023-07-06'),
+      location: 'ZN1',
+    },
+    {
+      id: 4,
+      code: 'PF4',
+      type: 'TP1',
+      description: 'Esta es la descripción de la familia del producto',
+      status: 'Activo',
+      creationDate: new Date('2023-07-06'),
+      location: 'ZN1',
+    },
+    {
+      id: 5,
+      code: 'PF5',
+      type: 'TP3',
+      description: 'Esta es la descripción de la familia del producto',
+      status: 'Inactivo',
+      creationDate: new Date('2023-07-06'),
+      location: 'ZN2',
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.equimentCount = this.equipments.length;
+  }
+
+  getStatusStyle(status: string): string[] {
+    switch (status) {
+      case 'Activo': return ['border-success', 'text-success', 'bg-success-subtle'];
+      case 'Inactivo': return ['border-danger', ' text-danger', 'bg-danger-subtle'];
+      default: return [];
+    }
+  }
+
+  changePage(pageToLoad: number): void {
+    this.currentPage = pageToLoad;
+  }
+}
