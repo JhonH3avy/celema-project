8ccb496db258cdd114d73b2b30aff156bf71411e
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-10-31 07:59:27 -0500

    Add Product (SKU) Admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileAdminComponent } from './features/general/profile-admin/profile-admin.component';
 import { RoleAdminComponent } from './features/general/role-admin/role-admin.component';
+import { ProductAdminComponent } from './features/general/product-admin/product-admin.component';
 
 const routes: Routes = [
   { path: 'administracion-perfiles', component: ProfileAdminComponent },
   { path: 'administracion-roles', component: RoleAdminComponent },
+  { path: 'administracion-productos', component: ProductAdminComponent },
 ];
 
 @NgModule({
