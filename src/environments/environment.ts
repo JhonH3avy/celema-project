e43ae68ce1f9d8340117bec2dd2fe7d2e22c958d
maospina24 <miguel24ospina1@gmail.com>
2024-11-09 12:05:31 -0500

    configuracion services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://62.146.180.148:81/api' // URL de la API para el entorno de desarrollo
 };
 
 /*
