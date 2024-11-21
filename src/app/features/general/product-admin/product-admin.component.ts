4c56857e3416fa1b912a69a1557c18578efed8d2
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-11-21 18:00:03 -0500

    integrate product admin

--- src/app/features/general/product-admin/product-admin.component.ts
+++ src/app/features/general/product-admin/product-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ProductoDto, ProductosService } from 'src/app/core/services';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-product-admin',
@@ -11,76 +14,106 @@ export class ProductAdminComponent implements OnInit {
 
   currentPage = 1;
   totalPages = 5;
-  pages = Array(this.totalPages).fill(0);
+  pages: number[] = [];
 
-  products = [
-    {
-      id: 1,
-      code: 'PRDCT1',
-      description: 'Esta es la descripción del producto',
-      status: 'Activo',
-      creationDate: new Date('2023-07-06'),
-      volume: '3 lts',
-      presentation: 'Caja',
-      family: 'Lácteos',
-    },
-    {
-      id: 2,
-      code: 'PRDCT2',
-      description: 'Esta es la descripción del producto',
-      status: 'Activo',
-      creationDate: new Date('2023-07-06'),
-      volume: '3 lts',
-      presentation: 'Caja',
-      family: 'Lácteos',
-    },
-    {
-      id: 3,
-      code: 'PRDCT3',
-      description: 'Esta es la descripción del producto',
-      status: 'Activo',
-      creationDate: new Date('2023-07-06'),
-      volume: '3 lts',
-      presentation: 'Caja',
-      family: 'Lácteos',
-    },
-    {
-      id: 4,
-      code: 'PRDCT4',
-      description: 'Esta es la descripción del producto',
-      status: 'Activo',
-      creationDate: new Date('2023-07-06'),
-      volume: '3 lts',
-      presentation: 'Caja',
-      family: 'Lácteos',
-    },
-    {
-      id: 5,
-      code: 'PRDCT5',
-      description: 'Esta es la descripción del producto',
-      status: 'Inactivo',
-      creationDate: new Date('2023-07-06'),
-      volume: '3 lts',
-      presentation: 'Caja',
-      family: 'Lácteos',
-    },
-  ]
+  products: ProductoDto[] = [];
+  filteredData: ProductoDto[] = [];
+  paginatedData: ProductoDto[] = [];
 
-  constructor() { }
+  itemsPerPageControl = new FormControl(10);
+  searchQuery = new FormControl();
+
+  private offsetPagesToDisplay = 5;
+
+  private get itemsPerPage(): number {
+    const itemsPerPageValue = this.itemsPerPageControl.value;
+    if (typeof itemsPerPageValue === 'string') {
+      return Number.parseInt(itemsPerPageValue);
+    } if (typeof itemsPerPageValue === 'number') {
+      return itemsPerPageValue;
+    }
+    return 0;
+  }
+
+  constructor(
+    private productService: ProductosService,
+  ) { }
 
   ngOnInit(): void {
     this.productCount = this.products.length;
+    this.getData();
+    this.searchQuery.valueChanges.subscribe(query => this.filterData(query));
   }
 
-  getStatusStyle(status: string): string[] {
+  getData(): void {
+    this.productService.apiProductosBuscarTodosGet()
+      .subscribe(response => {
+        this.currentPage = 1;
+        this.products = response.datos ?? [];
+        this.filteredData = this.products;
+        this.productCount = this.filteredData.length;
+        this.totalPages = Math.ceil(this.productCount / this.itemsPerPage);
+        this.updatePagination();
+        this.updatePaginatedData();
+      });
+  }
+
+  filterData(query: string): void {
+    if (query.trim() === '') {
+      this.filteredData = this.products;
+    } else {
+      this.filteredData = this.products.filter(product =>
+        product.nombre?.toLowerCase().includes(query.toLowerCase())
+      );
+      this.currentPage = 1;
+    }
+    this.productCount = this.filteredData.length;
+    this.totalPages = Math.ceil(this.productCount / this.itemsPerPage);
+    this.updatePaginatedData();
+    this.updatePagination();
+  }
+
+  getStatusStyle(status: boolean | undefined): string[] {
     switch (status) {
-      case 'Activo': return ['border-success', 'text-success', 'bg-success-subtle'];
-      case 'Inactivo': return ['border-danger', ' text-danger', 'bg-danger-subtle'];
+      case true: return ['border-success', 'text-success', 'bg-success-subtle'];
+      case false: return ['border-danger', ' text-danger', 'bg-danger-subtle'];
       default: return [];
     }
   }
 
   changePage(pageToLoad: number): void {
     this.currentPage = pageToLoad;
+    this.updatePaginatedData();
+    this.updatePagination();
+  }
+
+  changeItemsPerPage(): void {
+    this.totalPages = Math.ceil(this.productCount / this.itemsPerPage);
+    this.currentPage = 1;
+    this.updatePaginatedData();
+    this.updatePagination();
+  }
+
+  updatePaginatedData(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedData = this.filteredData.slice(startIndex, endIndex); // Usa filteredData en lugar de data
+  }
+
+  updatePagination(): void {
+    this.pages = [];
+    const initialPage = Math.max(1, this.currentPage - this.offsetPagesToDisplay);
+    const negativeOffset = this.currentPage - this.offsetPagesToDisplay < 0 ? this.currentPage - this.offsetPagesToDisplay : 0;
+    const finalPage = Math.min(this.totalPages, this.currentPage + this.offsetPagesToDisplay - negativeOffset);
+    for (let i = initialPage; i <= finalPage; i++) {
+      this.pages.push(i);
+    }
+  }
+
+  exportToExcel(): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.products);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Roles');
+    XLSX.writeFile(wb, 'productos.xlsx');
   }
 }
