73c12938eb3aaf18291890165f4f62a52988e628
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-11-22 23:15:31 -0500

    re-generate both versions

--- generate_swagger_api.ps1
+++ generate_swagger_api.ps1
@@ -1,12 +1,1 @@
-# Define the URL of the Swagger JSON file
-$url = "http://62.146.180.148:82/swagger/v1/swagger.json"
-
-# Define the path where the file should be saved
-$outputPath = "${PWD}\swagger.json"
-
-# Use Invoke-WebRequest to download the file
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-Write-Host "Swagger JSON file downloaded to $outputPath"
-
-docker run --rm -v ${PWD}:/usr/src/app openapitools/openapi-generator-cli generate -i /usr/src/app/swagger.json -g typescript-angular -o /usr/src/app/src/app/core/services-v2/ --skip-validate-spec
+docker run --rm -v ${PWD}:/usr/src/app openapitools/openapi-generator-cli generate -i /usr/src/app/swagger.json -g typescript-angular -o /usr/src/app/src/app/core/services/ --skip-validate-spec
