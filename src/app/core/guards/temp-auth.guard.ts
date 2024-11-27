ef05034f7693a73b110dfae4c81a89161a27c017
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-11-27 08:25:10 -0500

    simplify recover password guard logic

--- src/app/core/guards/temp-auth.guard.ts
+++ src/app/core/guards/temp-auth.guard.ts
@@ -4,16 +4,10 @@ import { CanActivateFn, Router } from '@angular/router';
 export const tempAuthGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
-  const token = localStorage.getItem('authToken');
-  const isAuthenticated = !!token;
-
   const tempToken = route.queryParams['token'];
   const isSettingPassword = !!tempToken;
 
-  const profileRegex = /reestablecer-contrasena/i;
-  const isActivatingProfile = route.url.some(segment => profileRegex.test(segment.path));
-
-  const canActivate = isAuthenticated || (isSettingPassword && isActivatingProfile);
+  const canActivate = isSettingPassword;
   if (!canActivate) {
     router.navigate(['/login']);
   }
