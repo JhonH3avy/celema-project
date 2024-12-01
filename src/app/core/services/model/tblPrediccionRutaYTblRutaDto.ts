1252db08a06f49370a36eaf7b3562addc3b43f4e
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2024-12-01 17:24:14 -0500

    integrate route planification

--- src/app/core/services/model/tblPrediccionRutaYTblRutaDto.ts
+++ src/app/core/services/model/tblPrediccionRutaYTblRutaDto.ts
@@ -13,9 +13,13 @@ export interface TblPrediccionRutaYTblRutaDto {
     id?: number;
     descripcion?: string | null;
     idFamilia?: number | null;
+    nombreFamilia?: string | null;
     tipoRuta?: string | null;
     linea?: number | null;
     restriccion?: number | null;
+    nombreRestriccion?: string | null;
+    precision?: number | null;
+    listaEquipos?: Array<string> | null;
     sugerencia?: boolean;
     mensaje?: string | null;
 }
--- src/app/features/general/route-planification-detail/route-planification-detail.component.ts
+++ src/app/features/general/route-planification-detail/route-planification-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl } from '@angular/forms';
+import { NgbModal, NgbModalOptions, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { TblPrediccionRutaYTblRutaDto, TblPrediccionRutaYTblRutaService } from 'src/app/core/services';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-route-planification-detail',
@@ -9,65 +12,56 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class RoutePlanificationDetailComponent {
   currentPage = 1;
   totalPages = 5;
-  pages = Array(this.totalPages).fill(0);
+  itemsPerPage = 10;
+  offsetPagesToDisplay = 5;
+  pages: number[] = [];
 
-  washRestrictions = [
-    {
-      id: 1,
-      code: 'PRDCT1',
-      description: 'Esta es la descripción del equipo',
-      status: 'OK',
-      requestDate: new Date('2023-07-06'),
-      equipmentList: ['EC1', 'EC4', 'EC2'],
-      restrictionList: ['RC1', 'RC5'],
-      routeUsage: '10',
-    },
-    {
-      id: 2,
-      code: 'PRDCT2',
-      description: 'Esta es la descripción del equipo',
-      status: 'OK',
-      requestDate: new Date('2023-07-06'),
-      equipmentList: ['EC1'],
-      restrictionList: ['RC1', 'RC2', 'RC3'],
-      routeUsage: '2',
-    },
-    {
-      id: 3,
-      code: 'PRDCT3',
-      description: 'Esta es la descripción del equipo',
-      status: 'OK',
-      requestDate: new Date('2023-07-06'),
-      equipmentList: ['EC3', 'EC4'],
-      restrictionList: ['RC2', 'RC8', 'RC10'],
-      routeUsage: '5',
-    },
-    {
-      id: 4,
-      code: 'PRDCT4',
-      description: 'Esta es la descripción del equipo',
-      status: 'OK',
-      requestDate: new Date('2023-07-06'),
-      equipmentList: ['EC1'],
-      restrictionList: ['RC1'],
-      routeUsage: '9',
-    },
-    {
-      id: 5,
-      code: 'PRDCT5',
-      description: 'Esta es la descripción del equipo',
-      status: 'OFF',
-      requestDate: new Date('2023-07-06'),
-      equipmentList: ['EC1', 'EC2', 'EC3'],
-      restrictionList: ['RC1', 'RC2', 'RC3'],
-      routeUsage: '8',
-    },
-  ]
+  data: TblPrediccionRutaYTblRutaDto[] = [];
 
-  constructor(private modalService: NgbModal) {
-   }
+  currentPlan: TblPrediccionRutaYTblRutaDto | null = null;
+
+  semanaControl = new FormControl();
+
+  semanaChooseModalRef: NgbModalRef | null = null;
+
+  loading = false;
+
+  constructor(
+    private modalService: NgbModal,
+    private routePlanificationDetailService: TblPrediccionRutaYTblRutaService,
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  getData(): void {
+    this.loading = true;
+    this.routePlanificationDetailService.apiTblPrediccionRutaYTblRutaObtenerRutasPrediccionesPaginadasGet(this.semanaControl.value, this.currentPage, this.itemsPerPage)
+      .subscribe(
+        response => {
+          this.data = response.datos ?? [];
+          this.totalPages = response.totalPaginas ?? 0;
+          this.semanaChooseModalRef?.dismiss();
+          this.updatePagination();
+        }, error => {
+          if (error.status === 400) {
+            Swal.fire({
+              title: 'Error',
+              text: error.error,
+              icon: 'error',
+            });
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: 'Sucedió un error inseperado al traer las predicciones de rutas',
+              icon: 'error',
+            });
+          }
+        }, () => {
+          this.loading = false;
+        }
+      );
   }
 
   getStatusStyle(status: string): string[] {
@@ -80,9 +74,32 @@ export class RoutePlanificationDetailComponent {
 
   changePage(pageToLoad: number): void {
     this.currentPage = pageToLoad;
+    this.data = [];
+    this.getData();
+    this.updatePagination();
   }
 
   openModal(modalContent: any): void {
-    this.modalService.open(modalContent, { size: 'lg', backdrop: 'static', centered: true });
+    const modalOptions = { size: 'lg', backdrop: 'static', centered: true } as NgbModalOptions;
+    this.modalService.open(modalContent, modalOptions);
+  }
+
+  openSemanaChooseModal(modalContent: any): void {
+    this.semanaControl.setValue('');
+    this.semanaChooseModalRef = this.modalService.open(modalContent);
+  }
+
+  updatePagination(): void {
+    this.pages = [];
+    const initialPage = Math.max(1, this.currentPage - this.offsetPagesToDisplay);
+    const negativeOffset = this.currentPage - this.offsetPagesToDisplay < 0 ? this.currentPage - this.offsetPagesToDisplay : 0;
+    const finalPage = Math.min(this.totalPages, this.currentPage + this.offsetPagesToDisplay - negativeOffset);
+    for (let i = initialPage; i <= finalPage; i++) {
+      this.pages.push(i);
+    }
+  }
+
+  selectPlan(plan: TblPrediccionRutaYTblRutaDto): void {    
+    this.currentPlan = plan;
   }
 }
